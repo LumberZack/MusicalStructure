3af47a627da2d1a8afe30734c16de259f46a3063
LumberZack <zack.poirier88@gmail.com>
2016-08-07 11:51:42 -0400

    Layout Design
    Designed the general layout as well as implemented the whole layout and
    assets of the library activity.
    Also tested and implemented changing activities.

--- app/src/main/java/com/example/android/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Finding the Albums Button
+        View albums = findViewById(R.id.albumView);
+        //Establishing Event Listener to change the activity
+        albums.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent albumsIntent = new Intent(MainActivity.this, Albums.class);
+                startActivity(albumsIntent);
+            }
+        });
     }
 }
